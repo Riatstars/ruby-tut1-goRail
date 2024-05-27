8c9210fff52681a01cbe66ca79682f26bc4f47ae
“nampnt” <nampnt@smartosc.com>
2024-05-27 17:41:10 +0700

    part 21 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get "about-us",to: "about#index", as: :about
-  root to:  "main#index"
+  get 'about-us',to: 'about#index', as: :about
+  root to: 'main#index'
 
-  get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrations#create"
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
 
-  get "sign_in", to: "sessions#new"
-  post "sign_in", to: "sessions#create"
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 
-  delete "logout", to: "sessions#destroy"
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+  
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
 
 
-  get "up" => "rails/health#show", as: :rails_health_check
+
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
